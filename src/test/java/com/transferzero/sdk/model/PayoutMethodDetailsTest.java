cf782ca8a7451cbf5dae56faeeaddfaa37253c5b
Emanuele Gorga <emanuele@bitpesa.co>
2020-03-12 15:25:32 +0000

    BTA-3102 Regenerate manually SDK

--- src/test/java/com/transferzero/sdk/model/PayoutMethodDetailsTest.java
+++ src/test/java/com/transferzero/sdk/model/PayoutMethodDetailsTest.java
@@ -19,6 +19,7 @@ import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import com.transferzero.sdk.model.PayoutMethodBankAccountTypeEnum;
+import com.transferzero.sdk.model.PayoutMethodDetailsBTC;
 import com.transferzero.sdk.model.PayoutMethodDetailsBalance;
 import com.transferzero.sdk.model.PayoutMethodDetailsGHSBank;
 import com.transferzero.sdk.model.PayoutMethodDetailsIBAN;
