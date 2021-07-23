041de1c64059cf99f09046fe307f36467e1ead1c
Gadiza Zerari <gadisha@bitpesa.co>
2021-07-23 11:03:39 +0100

    BTA-6591 Release java sdk version 1.15.0 (#23)
    * BTA-6591 Release java sdk version 1.15.0
    
    ticket: https://azafinance.atlassian.net/browse/BTA-6591
    
    * Change version number

--- src/main/java/com/transferzero/sdk/ApiClient.java
+++ src/main/java/com/transferzero/sdk/ApiClient.java
@@ -103,7 +103,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("TransferZero-SDK/Java8/1.14.1-SNAPSHOT");
+        setUserAgent("TransferZero-SDK/Java8/1.15.0-SNAPSHOT");
     }
 
     /**
