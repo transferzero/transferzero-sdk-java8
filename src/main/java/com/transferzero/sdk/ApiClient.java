ff06bed21d9bd5c555aa0d2645e0226d2cb0e9f2
CodeBuild <>
2021-12-20 10:34:28 +0000

    BTA-7413 release transferzero sdk java8 to maven v1.18.0 (#30)
    * Change generated files
    
    * [maven-release-plugin] prepare release transferzero-sdk-java8-1.18.0
    
    * [maven-release-plugin] prepare for next development iteration

--- src/main/java/com/transferzero/sdk/ApiClient.java
+++ src/main/java/com/transferzero/sdk/ApiClient.java
@@ -103,7 +103,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("TransferZero-SDK/Java8/1.18.0-SNAPSHOT");
+        setUserAgent("TransferZero-SDK/Java8/1.18.1-SNAPSHOT");
     }
 
     /**
