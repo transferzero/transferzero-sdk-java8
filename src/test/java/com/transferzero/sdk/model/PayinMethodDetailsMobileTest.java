0afb9d33a15b5746f1a98a673ad52bb21bd24e45
Evanson Wachira <evansonwachira@gmail.com>
2022-08-15 13:47:36 +0300

    BTA-9289 1.21.0 release (#37)

--- src/test/java/com/transferzero/sdk/model/PayinMethodDetailsMobileTest.java
+++ src/test/java/com/transferzero/sdk/model/PayinMethodDetailsMobileTest.java
@@ -18,6 +18,7 @@ import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import com.transferzero.sdk.model.PayoutMethodCountryEnum;
 import com.transferzero.sdk.model.PayoutMethodMobileProviderEnum;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -55,6 +56,14 @@ public class PayinMethodDetailsMobileTest {
     @Test
     public void mobileProviderTest() {
         // TODO: test mobileProvider
+    }
+
+    /**
+     * Test the property 'country'
+     */
+    @Test
+    public void countryTest() {
+        // TODO: test country
     }
 
     /**
