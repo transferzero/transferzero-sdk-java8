1352d341deea8fb6eb24c4d5d2d6543ea7a1a60c
Evanson Wachira <evansonwachira@gmail.com>
2023-01-19 10:05:25 +0300

    BTA-10273 1.23.5 release

--- src/test/java/com/transferzero/sdk/model/PayoutMethodDetailsTest.java
+++ src/test/java/com/transferzero/sdk/model/PayoutMethodDetailsTest.java
@@ -47,6 +47,7 @@ import com.transferzero.sdk.model.PayoutMethodIdentityCardTypeEnum;
 import com.transferzero.sdk.model.PayoutMethodLegalEntityTypeEnum;
 import com.transferzero.sdk.model.PayoutMethodMobileProviderEnum;
 import com.transferzero.sdk.model.PayoutMethodNatureOfBusinessEnum;
+import com.transferzero.sdk.model.PayoutMethodPixKeyTypeEnum;
 import com.transferzero.sdk.model.PayoutMethodTransferReasonEnum;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
