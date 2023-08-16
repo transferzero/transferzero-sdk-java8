a12753596c5bf552fd17962084610d37f808537d
CodeBuild <>
2023-08-16 11:26:35 +0000

    BTA-11835 Remove double SNAPSHOT (#59)

--- src/main/java/com/transferzero/sdk/ApiClient.java
+++ src/main/java/com/transferzero/sdk/ApiClient.java
@@ -103,7 +103,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("TransferZero-SDK/Java8/1.29.0-SNAPSHOT");
+        setUserAgent("TransferZero-SDK/Java8/1.29.1-SNAPSHOT");
     }
 
     /**
