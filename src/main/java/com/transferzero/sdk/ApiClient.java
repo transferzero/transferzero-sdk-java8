21f2d4fca3afbd0560daa6307b39bb3f12806883
CodeBuild <>
2023-10-30 16:54:09 +0000

    Remove Account Debit Show and Index (#64)

--- src/main/java/com/transferzero/sdk/ApiClient.java
+++ src/main/java/com/transferzero/sdk/ApiClient.java
@@ -103,7 +103,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("TransferZero-SDK/Java8/1.31.0-SNAPSHOT");
+        setUserAgent("TransferZero-SDK/Java8/1.32.0-SNAPSHOT");
     }
 
     /**
