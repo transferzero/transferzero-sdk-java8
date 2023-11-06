b0030f21ce249b4dd46cc21e3e47d4de69706e47
CodeBuild <>
2023-11-06 11:30:22 +0000

    BTA-12239-1.32.1-Release (#66)

--- src/main/java/com/transferzero/sdk/ApiClient.java
+++ src/main/java/com/transferzero/sdk/ApiClient.java
@@ -103,7 +103,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("TransferZero-SDK/Java8/1.32.1-SNAPSHOT");
+        setUserAgent("TransferZero-SDK/Java8/1.33.0-SNAPSHOT");
     }
 
     /**
